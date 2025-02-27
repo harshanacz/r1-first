a36fc5b3fdd894434ffc00ee876da3aaf0cbe74f
Kavindu Harshana <45476434+harshanacz@users.noreply.github.com>
2025-02-27 19:17:35 +0530

    etrack-select added

--- etrack/src/App.jsx
+++ etrack/src/App.jsx
@@ -1,15 +1,18 @@
 import Header from "./components/Header";
 import ExpenseForm from "./components/ExpenseForm"
 import { useState } from "react";
+import ExpenseTile from "./components/ExpenseTile";
 
 function App() {
   const [isFormAvailable, setisFormAvailable] = useState(false);
 
   return (
-    <div>
+    <div className="lg:mx-20 mx-5">
       <Header isFormAvailable={isFormAvailable}
         setisFormAvailable={setisFormAvailable} />
       {isFormAvailable && <ExpenseForm />}
+
+      <ExpenseTile/>
     </div>
   );
 }
---
+++ etrack/src/components/ExpenseTile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExpenseTile = () => {
+  return (
+    <div className="flex flex-row">
+    <h1>Hello</h1>
+    <h1>aaa</h1>
+</div>
+  )
+}
+
+export default ExpenseTile
